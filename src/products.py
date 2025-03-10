7b097ddda62097b47487ab672c66d069c6433ea3
Alyona Sytnik <alyona-sytnik0908@yandex.ru>
2025-03-10 21:16:50 +0200

    Add functions and tests

---
+++ src/products.py
@@ -0,0 +1,18 @@
+class Product:
+    """
+    Класс для описания товара в магазине
+    """
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    product_count = 0
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+        Product.product_count += 1
