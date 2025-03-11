8f20349dfec33193d517351be73d6f94b5032113
Alyona Sytnik <alyona-sytnik0908@yandex.ru>
2025-03-11 18:35:29 +0200

    Add coverage

--- src/products.py
+++ src/products.py
@@ -8,12 +8,8 @@ class Product:
     price: float
     quantity: int
 
-    product_count = 0
-
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
-
-        Product.product_count += 1
