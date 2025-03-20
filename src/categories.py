f5d12f5b406645a5113748e4b13f6b6fda866feb
Alyona Sytnik <alyona-sytnik0908@yandex.ru>
2025-03-20 23:21:29 +0200

    Change categories.py

--- src/categories.py
+++ src/categories.py
@@ -1,3 +1,6 @@
+from src.products import Product
+
+
 class Category:
     """
     Класс для категорий товара
@@ -12,7 +15,23 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
-
+        self.__products = products
         Category.category_count += 1
-        Category.product_count += len(self.products)
+
+    def add_product(self, product: Product):
+        if not isinstance(product, Product):
+            raise TypeError("Добавляемый объект должен быть экземпляром класса Product или его наследников.")
+
+        self.__products.append(product)
+        Category.product_count = len(self.__products)
+
+    @property
+    def products(self):
+        return self.__products
+
+    @property
+    def products_str(self):
+        products_str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
